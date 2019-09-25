7e987b70937eac71cc20ec2d803cfcc0fc6252cd
pauljosephwilson <pauljosephwilson@utexas.edu>
2019-09-24 19:52:03 -0500

    Added date functionality and lump sum

--- bot.py
+++ bot.py
@@ -1,12 +1,4 @@
-
-
-
-import tweepy
-from datetime import date
-import datetime
-
-
-
+# authorizations for bot
 consumer_key= 'A7W4xgF21W7qAa23u7V7hhvIC'
 consumer_secret='xIqtm7fJgga0BFjX4IE4ZOTQPLNQBPuWK8VAwcubvbMpViNw07'
 access_token = '1098063757006254080-mev9NsOgYXUKBjHWy6F5VrJJLnXpEw'
@@ -16,7 +8,7 @@ auth= tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 api= tweepy.API(auth)
 
-
+#calculate first tweet
 d0= date(2018,1,1)
 d1= date.today()
 daysbetween= abs(d1-d0)
@@ -33,12 +25,10 @@ buyout= int(buyoutcalc)
 
 lumpsum= buyout*.25
 
+def moneyupdate():
+    api.update_status('If he were fired today, Jimbo Fisher would be owed $'+ str('{:,.2f}'.format(buyout))+ ' by Texas A&M, with $' +str('{:,.2f}'.format(lumpsum))+' due by '+ lumpdate.strftime('%B %d')+' #12thman #wrts #gigemgang2020')
+    print('done')
 
-print(buyout)
-print(lumpsum)
-print(lumpdate.strftime('%B %d'))
-user=api.me()
-print(user.name)
 
-api.update_status('If he were fired today, Jimbo Fisher would be owed $'+ str('{:,.2f}'.format(buyout))+ ' by Texas A&M, with $' +str('{:,.2f}'.format(lumpsum))+' due by '+ lumpdate.strftime('%B %d')+' #12thman #wrts #gigemgang2020')
-print('done')
+
+
