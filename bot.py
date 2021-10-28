48605d62f484507413763a7f9e59c15556d4d802
pauljosephwilson <43251534+pauljosephwilson@users.noreply.github.com>
2021-10-28 02:26:47 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,10 @@ from sportsreference.ncaaf.teams import Teams
 
 
 # authorizations for bot
-consumer_key= 'A7W4xgF21W7qAa23u7V7hhvIC'
-consumer_secret='xIqtm7fJgga0BFjX4IE4ZOTQPLNQBPuWK8VAwcubvbMpViNw07'
-access_token = '1098063757006254080-mev9NsOgYXUKBjHWy6F5VrJJLnXpEw'
-access_token_secret = 'XOud0ZDc9kmNFviCavHXxpo7PX7BCSQGDXnJMQsKwHUaJ'
+consumer_key= 'key'
+consumer_secret='key'
+access_token = 'key'
+access_token_secret = 'key'
 
 auth= tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
